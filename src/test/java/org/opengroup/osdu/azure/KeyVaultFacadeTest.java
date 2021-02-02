a822c990716a63d71888bbf79ff08cfd7cb5f191
Kelly Domico <kedomico@microsoft.com>
2021-02-01 19:06:40 -0800

    Mock coreloggerfactory in keyavultfacadetest

--- src/test/java/org/opengroup/osdu/azure/KeyVaultFacadeTest.java
+++ src/test/java/org/opengroup/osdu/azure/KeyVaultFacadeTest.java
@@ -16,21 +16,78 @@ package org.opengroup.osdu.azure;
 
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opengroup.osdu.azure.logging.CoreLogger;
+import org.opengroup.osdu.azure.logging.CoreLoggerFactory;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
 class KeyVaultFacadeTest {
     @Mock
+    private CoreLoggerFactory coreLoggerFactory;
+
+    @Mock
+    private CoreLogger coreLogger;
+
+    @Mock
     private SecretClient kv;
+
+    /**
+     * Workaround for inability to mock static methods like getInstance().
+     *
+     * @param mock CoreLoggerFactory mock instance
+     */
+    private void mockSingleton(CoreLoggerFactory mock) {
+        try {
+            Field instance = CoreLoggerFactory.class.getDeclaredField("instance");
+            instance.setAccessible(true);
+            instance.set(null, mock);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Reset workaround for inability to mock static methods like getInstance().
+     */
+    private void resetSingleton() {
+        try {
+            Field instance = CoreLoggerFactory.class.getDeclaredField("instance");
+            instance.setAccessible(true);
+            instance.set(null, null);
+            instance.setAccessible(false);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void init() {
+        mockSingleton(coreLoggerFactory);
+        when(coreLoggerFactory.getLogger(anyString())).thenReturn(coreLogger);
+    }
+
+    @AfterEach
+    public void takeDown() {
+        resetSingleton();
+    }
 
     @Test
     void getSecretWithValidation_checksForNullResponse() {
