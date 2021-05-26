5886afd4635eb89906918eb6954fac1ef5e8ad83
Ronak Sakhuja <rosakhuj@microsoft.com>
2021-05-27 00:39:19 +0530

    lint fixes

--- src/main/java/org/opengroup/osdu/azure/di/BlobStoreRetryConfiguration.java
+++ src/main/java/org/opengroup/osdu/azure/di/BlobStoreRetryConfiguration.java
@@ -19,8 +19,6 @@ import com.azure.storage.common.policy.RetryPolicyType;
 import lombok.Getter;
 import lombok.Setter;
 import org.opengroup.osdu.core.common.logging.ILogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
