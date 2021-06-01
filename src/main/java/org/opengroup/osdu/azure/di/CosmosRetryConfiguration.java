a5f2fb405757e163b99b094f13b5ad6175aee77a
Ronak Sakhuja <rosakhuj@microsoft.com>
2021-06-01 21:23:41 +0530

    Replaced default value of -1

--- src/main/java/org/opengroup/osdu/azure/di/CosmosRetryConfiguration.java
+++ src/main/java/org/opengroup/osdu/azure/di/CosmosRetryConfiguration.java
@@ -1,16 +1,17 @@
-// Copyright © Microsoft Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/** Copyright © Microsoft Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+**/
 
 package org.opengroup.osdu.azure.di;
 
@@ -32,31 +33,17 @@ import java.time.Duration;
 @Setter
 public class CosmosRetryConfiguration {
     public static final String LOGGER_NAME = CosmosRetryConfiguration.class.getName();
-    public static final int DEFAULT_INT_VALUE = -1;
+
+    private ThrottlingRetryOptions defaultThrottlingRetryOptions = new ThrottlingRetryOptions();
+
     /**
      * Value for max Retry Count on Throttled Requests for Cosmos.
      */
-    private int maxRetryCount = DEFAULT_INT_VALUE; // Setting default value of -1, indicates to use default maxRetryCount
+    private int maxRetryCount = defaultThrottlingRetryOptions.getMaxRetryAttemptsOnThrottledRequests(); // Sets the  default value for axRetryAttemptsOnThrottledRequests
     /**
      * Value for max retry wait time for Cosmos (Value in seconds).
      */
-    private long retryWaitTimeout = DEFAULT_INT_VALUE; // Setting default value of -1, indicates to use default retryWaitTimeout
-
-    /**
-     *
-     * @return Returns true if MaxRetryCount is configured.
-     */
-    public boolean isMaxRetryCountConfigured() {
-        return maxRetryCount != DEFAULT_INT_VALUE;
-    }
-
-    /**
-     *
-     * @return Returns true if RetryWaitTimeout is configured.
-     */
-    public boolean isRetryWaitTimeoutConfigured() {
-        return retryWaitTimeout != DEFAULT_INT_VALUE;
-    }
+    private long retryWaitTimeout = defaultThrottlingRetryOptions.getMaxRetryWaitTime().getSeconds(); // Sets the  default value for MaxRetryWaitTime in seconds
 
     /**
      * Set's the Throttling retry options based on application.properties configuration.
@@ -65,12 +52,9 @@ public class CosmosRetryConfiguration {
     public ThrottlingRetryOptions getThrottlingRetryOptions() {
         ThrottlingRetryOptions throttlingRetryOptions = new ThrottlingRetryOptions();
 
-        if (isMaxRetryCountConfigured()) {
-            throttlingRetryOptions.setMaxRetryAttemptsOnThrottledRequests(this.getMaxRetryCount());
-        }
-        if (isRetryWaitTimeoutConfigured()) {
-            throttlingRetryOptions.setMaxRetryWaitTime(Duration.ofSeconds(this.getRetryWaitTimeout()));
-        }
+        throttlingRetryOptions.setMaxRetryAttemptsOnThrottledRequests(this.getMaxRetryCount());
+        throttlingRetryOptions.setMaxRetryWaitTime(Duration.ofSeconds(this.getRetryWaitTimeout()));
+
         CoreLoggerFactory.getInstance().getLogger(LOGGER_NAME).info("Retry Options on CosmosClient with maxRetryAttempts = {} , MaxRetryWaitTime = {}.", throttlingRetryOptions.getMaxRetryAttemptsOnThrottledRequests(), throttlingRetryOptions.getMaxRetryWaitTime());
 
         return throttlingRetryOptions;
