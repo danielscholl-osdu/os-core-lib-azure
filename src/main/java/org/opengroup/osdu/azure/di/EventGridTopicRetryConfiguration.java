806734860d9e54d0d644f3d791e238830ee0bcf3
Ronak Sakhuja <rosakhuj@microsoft.com>
2021-06-29 15:49:16 +0530

    Added check for negative values

--- src/main/java/org/opengroup/osdu/azure/di/EventGridTopicRetryConfiguration.java
+++ src/main/java/org/opengroup/osdu/azure/di/EventGridTopicRetryConfiguration.java
@@ -1,7 +1,20 @@
+/** Copyright © Microsoft Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ **/
+
 package org.opengroup.osdu.azure.di;
 
-
-import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +24,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConfigurationProperties("azure.eventgrid.topic")
-@Getter
 @Setter
 public class EventGridTopicRetryConfiguration {
 
@@ -19,6 +31,16 @@ public class EventGridTopicRetryConfiguration {
 
     private int longRunningOperationRetryTimeout = DEFAULT_INT_VALUE;
 
+    /**
+     * Fetches value of Long Running Operation Retry Timeout.
+     * @return Value of longRunningOperationRetryTimeout greater than 0, else -1
+     */
+    public int getLongRunningOperationRetryTimeout() {
+        if (this.longRunningOperationRetryTimeout < 0) {
+            return DEFAULT_INT_VALUE;
+        }
+        return this.longRunningOperationRetryTimeout;
+    }
     /**
      * Used to check if timeout is configured in application.properties.
      * @return True if retry is configured.
