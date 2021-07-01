1d7e20aa87762774c09df6249e7b12021be1a833
SmithaManjunath <smanjunath@microsoft.com>
2021-07-01 15:10:37 +0530

    updated notice

--- src/test/java/org/opengroup/osdu/azure/health/AzureHealthEndpointWebExtensionTest.java
+++ src/test/java/org/opengroup/osdu/azure/health/AzureHealthEndpointWebExtensionTest.java
@@ -1,6 +1,5 @@
 package org.opengroup.osdu.azure.health;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
