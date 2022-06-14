746ec827f9fd283e5faf24d8e418013e5118d556
HARSHEET SHAH <harsheetshah@microsoft.com>
2022-06-14 15:44:35 +0530

    Adding validation for private links

--- src/main/java/org/opengroup/osdu/azure/filters/PrivateLinkFilter.java
+++ src/main/java/org/opengroup/osdu/azure/filters/PrivateLinkFilter.java
@@ -1,13 +1,16 @@
 package org.opengroup.osdu.azure.filters;
 
+import org.apache.commons.validator.routines.InetAddressValidator;
 import org.opengroup.osdu.azure.privateLinks.ValidateDataLinks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ValidationException;
 import java.io.IOException;
+
 
 @Component
 @ConditionalOnProperty(value = "validate.privateLink.enabled", havingValue = "true", matchIfMissing = true)
@@ -21,12 +24,37 @@ public class PrivateLinkFilter implements Filter {
 
         // get the ip address
 
-        if(validateDataLinks.validateRequest(""))
-            filterChain.doFilter(servletRequest,servletResponse);
-        else{
-            throw new ValidationException("Validation of data link failed.");
+        // https://mkyong.com/java/how-to-get-client-ip-address-in-java/ ?
+
+        String ipAddress = getIpaddress(servletRequest);
+        InetAddressValidator inetAddressValidator =  InetAddressValidator.getInstance();
+
+        if(inetAddressValidator.isValidInet6Address(ipAddress)) {
+
+            //ipv4 vs ipv6 -> only for ipv6
+            if (validateDataLinks.validateRequest(""))
+                filterChain.doFilter(servletRequest, servletResponse);
+            else {
+                throw new ValidationException("Validation of data link failed.");
+            }
+        }
+        else
+            filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    public String getIpaddress(ServletRequest servletRequest){
+
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+
+        String ipAddress ="";
+        if (request != null) {
+            ipAddress = request.getHeader("X-FORWARDED-FOR");
+            if (ipAddress == null || "".equals(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+            }
         }
 
-
+        return ipAddress;
     }
 }
--- src/main/java/org/opengroup/osdu/azure/privateLinks/ValidateDataLinks.java
+++ src/main/java/org/opengroup/osdu/azure/privateLinks/ValidateDataLinks.java
@@ -2,34 +2,27 @@ package org.opengroup.osdu.azure.privateLinks;
 
 import com.azure.cosmos.models.SqlQuerySpec;
 import org.opengroup.osdu.azure.cosmosdb.CosmosStore;
+import org.opengroup.osdu.azure.cosmosdb.system.config.SystemCosmosConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.opengroup.osdu.azure.cosmosdb.system.config.SystemCosmosConfig;
-import org.opengroup.osdu.core.common.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-import org.springframework.scheduling.annotation.SchedulingConfiguration;
+import org.springframework.scheduling.annotation.Scheduled;
 
-import javax.xml.ws.Response;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@ConditionalOnProperty(value = "privateLinks.validateLinks.enabled", havingValue = "true", matchIfMissing = true)
+
 @EnableScheduling
 public class ValidateDataLinks {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ValidateDataLinks.class);
-    private final String COSMOS_DB="";
-    private final String DATA_PARTITION_ID="";
-    private final String COLLECTION="PrivateLinkCollection";
+    private final String COSMOS_DB = "";
+    private final String DATA_PARTITION_ID = "";
+    private final String COLLECTION = "PrivateLinkCollection";
 
     @Autowired
     SystemCosmosConfig systemCosmosConfig;
@@ -39,20 +32,20 @@ public class ValidateDataLinks {
 
     List<Long> cache = new ArrayList<>();
 
-   public  boolean validateRequest(String ipv6) throws UnknownHostException {
+    public boolean validateRequest(String ipv6) throws UnknownHostException {
         byte[] bytes = InetAddress.getByName(ipv6).getAddress();
-        String bits = new BigInteger(1,bytes).toString(2);
+        String bits = new BigInteger(1, bytes).toString(2);
 
-        if(bits.length() != 128){
+        if (bits.length() != 128) {
             LOGGER.warn("Ipv6 address is less than 128 bit");
             return false;
         }
-         if(bits.charAt(9) == '1'){
-             // fetch private link from ipv6 address. It starts from 17th bit and is 32 bit length
-                String privateLinkString  = ipv6.substring(16, 48);
-                Long privateLinkDecimal = Long.parseLong(privateLinkString,2);
+        if (bits.charAt(9) == '1') {
+            // fetch private link from ipv6 address. It starts from 17th bit and is 32 bit length
+            String privateLinkString = ipv6.substring(16, 48);
+            Long privateLinkDecimal = Long.parseLong(privateLinkString, 2);
 
-                //check if present in cosmosDB?
+            //check if present in cosmosDB?
 
              /*
                 we don't need to validate the storage pvt link
@@ -68,35 +61,37 @@ public class ValidateDataLinks {
                 }
               */
 
-             if( isPresentInCache(privateLinkDecimal))
-                 return true;
-             else{
+            if (isPresentInCache(privateLinkDecimal))
+                return true;
+            else {
                 Long privateLink = null;
                 /* call to db */
 
-                if(privateLink !=null)
+                if (privateLink != null) {
                     cache.add(privateLink);
+                    return true;
+                }
                 else {
                     LOGGER.error("Private link not found in DB");
                     return false;
                 }
-             }
             }
-         return false;
-
         }
+        return false;
+
+    }
 
     private boolean isPresentInCache(Long privateLinkDecimal) {
         return cache.contains(privateLinkDecimal);
     }
 
 
-    @Scheduled( fixedRate = 6000)
-    void cacheSyncUp(){
+    @Scheduled(fixedRate = 6000)
+    void cacheSyncUp() {
         String queryText = "SELECT * FROM c WHERE 1=1 ";
         SqlQuerySpec query = new SqlQuerySpec(queryText);
 
-        cache = cosmosStore.queryItems(COSMOS_DB,COLLECTION,query,null, Long.class);
+        cache = cosmosStore.queryItems(COSMOS_DB, COLLECTION, query, null, Long.class);
         LOGGER.info("Syncing up cache with DB");
 
     }
