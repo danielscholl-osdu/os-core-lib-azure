e32249f4dea2ffa16f207eda8cd59727b2a24ddf
Harsheet Shah <harsheetshah@microsoft.com>
2022-06-23 14:19:52 +0000

    Adding validation for private links

---
+++ src/main/java/org/opengroup/osdu/azure/privateLinks/PrivateLinkResponse.java
@@ -0,0 +1,12 @@
+package org.opengroup.osdu.azure.privateLinks;
+
+import lombok.Getter;
+/**
+Private link Response object.
+ */
+@Getter
+public class PrivateLinkResponse {
+
+    private String privateLinkID;
+    private String id;
+}
