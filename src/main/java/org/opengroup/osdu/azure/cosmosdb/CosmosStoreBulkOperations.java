c5423802cc0c390946f83ad80d2c859656520463
Alok Joshi <AJoshi19@slb.com>
2023-03-02 13:48:53 -0600

    bulk patch ops

--- src/main/java/org/opengroup/osdu/azure/cosmosdb/CosmosStoreBulkOperations.java
+++ src/main/java/org/opengroup/osdu/azure/cosmosdb/CosmosStoreBulkOperations.java
@@ -6,6 +6,7 @@ import com.azure.cosmos.models.CosmosBulkExecutionOptions;
 import com.azure.cosmos.models.CosmosBulkItemResponse;
 import com.azure.cosmos.models.CosmosBulkOperations;
 import com.azure.cosmos.models.CosmosItemOperation;
+import com.azure.cosmos.models.CosmosPatchOperations;
 import com.azure.cosmos.models.PartitionKey;
 import com.google.gson.Gson;
 import com.microsoft.azure.documentdb.DocumentClientException;
@@ -192,6 +193,101 @@ public class CosmosStoreBulkOperations {
             final DependencyLoggingOptions loggingOptions = DependencyLoggingOptions.builder()
                     .type(COSMOS_STORE)
                     .name("UPSERT_ITEMS")
+                    .data(dependencyData)
+                    .target(dependencyTarget)
+                    .timeTakenInMs(timeTaken)
+                    .requestCharge(requestCharge[0])
+                    .resultCode(statusCode[0])
+                    .success(status == HttpStatus.SC_OK)
+                    .build();
+            dependencyLogger.logDependency(loggingOptions);
+        }
+    }
+
+
+    /**
+     * Bulk patch itemes into cosmos collection using CosmosClient.
+     * Partition Keys must be provided in the same order as records.
+     * ith Record's partition Key will be at ith position in the List.
+     *
+     * @param dataPartitionId                 name of data partition.
+     * @param cosmosDBName                    name of Cosmos db.
+     * @param collectionName                  name of collection in Cosmos.
+     * @param docIds                          ids of the documents to be patched
+     * @param cosmosPatchOperations           CosmosPatchOperations to be performed on each document
+     * @param partitionKeys                   List of partition keys corresponding to "docs" provided
+     * @param maxConcurrencyPerPartitionRange concurrency per partition (1-5)
+     */
+    public final void bulkPatchWithCosmosClient(final String dataPartitionId,
+                                                     final String cosmosDBName,
+                                                     final String collectionName,
+                                                     final List<String> docIds,
+                                                     final CosmosPatchOperations cosmosPatchOperations,
+                                                     final List<String> partitionKeys,
+                                                     final int maxConcurrencyPerPartitionRange) {
+        final long start = System.currentTimeMillis();
+        final int[] statusCode = {HttpStatus.SC_OK};
+        final int[] clientStatusCode = {HttpStatus.SC_OK};
+        final double[] requestCharge = {0.0};
+        try {
+            List<String> exceptions = new ArrayList<>();
+
+            CosmosClient cosmosClient = cosmosClientFactory.getClient(dataPartitionId);
+            CosmosContainer container = cosmosClient.getDatabase(cosmosDBName).getContainer(collectionName);
+
+            List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
+            for (int i = 0; i < docIds.size(); i++) {
+                cosmosItemOperations.add(CosmosBulkOperations.getPatchItemOperation(docIds.get(i), new PartitionKey(partitionKeys.get(i)), cosmosPatchOperations));
+            }
+
+            CosmosBulkExecutionOptions cosmosBulkExecutionOptions = new CosmosBulkExecutionOptions();
+            cosmosBulkExecutionOptions.setMaxMicroBatchConcurrency(maxConcurrencyPerPartitionRange);
+
+            container.executeBulkOperations(cosmosItemOperations, cosmosBulkExecutionOptions).forEach(cosmosBulkOperationResponse -> {
+                CosmosBulkItemResponse cosmosBulkItemResponse = cosmosBulkOperationResponse.getResponse();
+                CosmosItemOperation cosmosItemOperation = cosmosBulkOperationResponse.getOperation();
+                requestCharge[0] += cosmosBulkItemResponse.getRequestCharge();
+
+                if (cosmosBulkItemResponse != null && cosmosBulkOperationResponse.getResponse() != null && cosmosBulkOperationResponse.getResponse().isSuccessStatusCode()) {
+                    LOGGER.info("Item : [{}], Status Code: {}, Request Charge: {}", cosmosItemOperation.getItem().toString(), cosmosBulkItemResponse.getStatusCode(), cosmosBulkItemResponse.getRequestCharge());
+                } else {
+                    LOGGER.error(
+                            "The operation for Item : [{}] Failed. Response code : {}. , Request Charge: {}, Exception : {}",
+                            cosmosItemOperation.getItem().toString(),
+                            cosmosBulkItemResponse.getStatusCode(),
+                            cosmosBulkItemResponse.getRequestCharge(),
+                            cosmosBulkOperationResponse.getException());
+                    if (cosmosBulkOperationResponse.getException() != null) {
+                        exceptions.add(cosmosBulkOperationResponse.getException().toString());
+                    } else {
+                        exceptions.add("Error occurred while patch operation");
+                    }
+                    if (cosmosBulkItemResponse.getStatusCode() >= 500) {
+                        statusCode[0] = cosmosBulkItemResponse.getStatusCode();
+                    } else if (cosmosBulkItemResponse.getStatusCode() >= 400) {
+                        clientStatusCode[0] = cosmosBulkItemResponse.getStatusCode();
+                    }
+                }
+            });
+
+            if (!exceptions.isEmpty()) {
+                int status = statusCode[0] != HttpStatus.SC_OK ? statusCode[0] : clientStatusCode[0];
+                LOGGER.error("Failed to patch documents in CosmosDB: {}", String.join(",", exceptions));
+                throw new AppException(status, "Record patch has failed!", "Failed to patch documents in CosmosDB", exceptions.toArray(new String[exceptions.size()]));
+            }
+        } catch (Exception e) {
+            int status = statusCode[0] != HttpStatus.SC_OK ? statusCode[0] : clientStatusCode[0];
+            String errorMessage = "Unexpectedly failed to bulk patch documents";
+            LOGGER.error(errorMessage, e);
+            throw new AppException(status, errorMessage, e.getMessage(), e);
+        } finally {
+            int status = statusCode[0] != HttpStatus.SC_OK ? statusCode[0] : clientStatusCode[0];
+            final long timeTaken = System.currentTimeMillis() - start;
+            final String dependencyTarget = DependencyLogger.getCosmosDependencyTarget(cosmosDBName, collectionName);
+            final String dependencyData = String.format("partition_key=%s", new HashSet<>(partitionKeys));
+            final DependencyLoggingOptions loggingOptions = DependencyLoggingOptions.builder()
+                    .type(COSMOS_STORE)
+                    .name("PATCH_ITEMS")
                     .data(dependencyData)
                     .target(dependencyTarget)
                     .timeTakenInMs(timeTaken)
