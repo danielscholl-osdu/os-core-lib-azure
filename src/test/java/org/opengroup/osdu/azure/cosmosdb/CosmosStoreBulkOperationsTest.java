408c9236fe79afdc7937b6f06805e441423e8cfb
Yurii Kondakov <ykondakov@slb.com>
2024-01-04 15:52:45 +0200

    add unit test

--- src/test/java/org/opengroup/osdu/azure/cosmosdb/CosmosStoreBulkOperationsTest.java
+++ src/test/java/org/opengroup/osdu/azure/cosmosdb/CosmosStoreBulkOperationsTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,13 +20,15 @@ import org.opengroup.osdu.azure.logging.DependencyLogger;
 import org.opengroup.osdu.azure.logging.DependencyLoggingOptions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -40,9 +43,29 @@ public class CosmosStoreBulkOperationsTest {
     private static final String DATA_PARTITION_ID = "data-partition-id";
 
     @Mock
+    CosmosClient cosmosClient;
+    @Mock
+    CosmosContainer cosmosContainer;
+    @Mock
+    CosmosPatchOperations cosmosPatchOperations;
+    @Mock
+    CosmosItemOperation cosmosItemOperation;
+    @Mock
+    CosmosBulkExecutionOptions cosmosBulkExecutionOptions;
+    @Mock
+    CosmosBulkOperationResponse cosmosBulkOperationResponse;
+    @Mock
+    CosmosBulkItemResponse cosmosBulkItemResponse;
+    @Mock
+    CosmosDatabase cosmosDatabase;
+    @Mock
     private DependencyLogger dependencyLogger;
     @Mock
     private ICosmosClientFactory cosmosClientFactory;
+    @Captor
+    ArgumentCaptor<DependencyLoggingOptions> loggingOptionsArgumentCaptor;
+    @Captor
+    ArgumentCaptor<List<CosmosItemOperation>> cosmosItemOperations;
 
     @InjectMocks
     private CosmosStoreBulkOperations sut;
@@ -50,19 +73,23 @@ public class CosmosStoreBulkOperationsTest {
     @BeforeEach
     void init() {
         openMocks(this);
+
+        lenient().doReturn(cosmosClient).when(cosmosClientFactory).getClient(DATA_PARTITION_ID);
+        lenient().doReturn(cosmosBulkItemResponse).when(cosmosBulkOperationResponse).getResponse();
+        lenient().doReturn(cosmosItemOperation).when(cosmosBulkOperationResponse).getOperation();
+        lenient().when(cosmosBulkOperationResponse.getResponse().isSuccessStatusCode()).thenReturn(true);
+        lenient().when(cosmosItemOperation.getItem()).thenReturn("item");
+        lenient().when(cosmosBulkItemResponse.getStatusCode()).thenReturn(200);
+        lenient().when(cosmosBulkItemResponse.getRequestCharge()).thenReturn(0.0);
+        when(cosmosClient.getDatabase(COSMOS_DB)).thenReturn(cosmosDatabase);
+        when(cosmosDatabase.getContainer(COLLECTION)).thenReturn(cosmosContainer);
+        cosmosBulkExecutionOptions.setMaxMicroBatchConcurrency(1);
     }
 
     @Test
-    public void bulkPatch_Success() {
-        CosmosClient cosmosClient = mock(CosmosClient.class);
-        CosmosContainer cosmosContainer = mock(CosmosContainer.class);
-        CosmosPatchOperations cosmosPatchOperations = mock(CosmosPatchOperations.class);
-        CosmosItemOperation cosmosItemOperation = mock(CosmosItemOperation.class);
-        CosmosBulkExecutionOptions cosmosBulkExecutionOptions = mock(CosmosBulkExecutionOptions.class);
-        cosmosBulkExecutionOptions.setMaxMicroBatchConcurrency(1);
+    void bulkPatch_Success() {
         List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
         cosmosItemOperations.add(cosmosItemOperation);
-        CosmosBulkOperationResponse cosmosBulkOperationResponse = mock(CosmosBulkOperationResponse.class);
         List<CosmosBulkOperationResponse> bulkPatchResponse = new ArrayList<>();
         bulkPatchResponse.add(cosmosBulkOperationResponse);
         Map<String, CosmosPatchOperations> cosmosPatchOperationsPerDoc = new HashMap<>();
@@ -70,19 +97,33 @@ public class CosmosStoreBulkOperationsTest {
         Map<String, String> partitionKeyForDoc = new HashMap<>();
         partitionKeyForDoc.put("id1", "id1");
 
-        lenient().doReturn(cosmosClient).when(cosmosClientFactory).getClient(DATA_PARTITION_ID);
-        CosmosBulkItemResponse cosmosBulkItemResponse = mock(CosmosBulkItemResponse.class);
-        lenient().doReturn(cosmosBulkItemResponse).when(cosmosBulkOperationResponse).getResponse();
         lenient().doReturn(bulkPatchResponse).when(cosmosContainer).executeBulkOperations(cosmosItemOperations, cosmosBulkExecutionOptions);
-        CosmosDatabase cosmosDatabase = mock(CosmosDatabase.class);
-        when(cosmosClient.getDatabase(COSMOS_DB)).thenReturn(cosmosDatabase);
-        when(cosmosDatabase.getContainer(COLLECTION)).thenReturn(cosmosContainer);
-        ArgumentCaptor<DependencyLoggingOptions> loggingOptionsArgumentCaptor = ArgumentCaptor.forClass(DependencyLoggingOptions.class);
 
         this.sut.bulkPatchWithCosmosClient(DATA_PARTITION_ID, COSMOS_DB, COLLECTION, cosmosPatchOperationsPerDoc, partitionKeyForDoc, 1);
 
         verify(this.cosmosClientFactory, times(1)).getClient(DATA_PARTITION_ID);
         verify(dependencyLogger, times(1)).logDependency(loggingOptionsArgumentCaptor.capture());
+        DependencyLoggingOptions actualLoggingOptions = loggingOptionsArgumentCaptor.getValue();
+        verifyDependencyLogging(actualLoggingOptions, "PATCH_ITEMS", "partition_key=[id1]", "cosmosdb/collection", 0.0, 200, true);
+    }
+
+    @Test
+    void bulkMultiPatch_Success() {
+        Map<String, List<CosmosPatchOperations>> cosmosPatchOperationsPerDoc = new HashMap<>();
+        cosmosPatchOperationsPerDoc.put("id1", new ArrayList<>(Arrays.asList(cosmosPatchOperations, cosmosPatchOperations)));
+        Map<String, String> partitionKeyForDoc = new HashMap<>();
+        partitionKeyForDoc.put("id1", "id1");
+        List<CosmosBulkOperationResponse> bulkPatchResponse = new ArrayList<>();
+        bulkPatchResponse.add(cosmosBulkOperationResponse);
+
+        lenient().doReturn(bulkPatchResponse).when(cosmosContainer).executeBulkOperations(anyList(), any(CosmosBulkExecutionOptions.class));
+
+        sut.bulkMultiPatchWithCosmosClient(DATA_PARTITION_ID, COSMOS_DB, COLLECTION, cosmosPatchOperationsPerDoc, partitionKeyForDoc, 1);
+        verify(cosmosClientFactory).getClient(DATA_PARTITION_ID);
+        verify(cosmosContainer).executeBulkOperations(cosmosItemOperations.capture(), any(CosmosBulkExecutionOptions.class));
+        List<CosmosItemOperation> itemOperations = cosmosItemOperations.getValue();
+        assertEquals(2, itemOperations.size());
+        verify(dependencyLogger).logDependency(loggingOptionsArgumentCaptor.capture());
         DependencyLoggingOptions actualLoggingOptions = loggingOptionsArgumentCaptor.getValue();
         verifyDependencyLogging(actualLoggingOptions, "PATCH_ITEMS", "partition_key=[id1]", "cosmosdb/collection", 0.0, 200, true);
     }
