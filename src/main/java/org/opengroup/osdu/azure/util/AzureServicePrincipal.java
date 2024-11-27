f29aa2ee25d1b0a387b4bfe2a3a994e51c2bc0cf
danielscholl <dascholl@microsoft.com>
2024-11-27 17:23:19 -0600

    Bug fix for default credentials to apply proper scope.

--- src/main/java/org/opengroup/osdu/azure/util/AzureServicePrincipal.java
+++ src/main/java/org/opengroup/osdu/azure/util/AzureServicePrincipal.java
@@ -77,7 +77,7 @@ public final class AzureServicePrincipal {
      * @return AUTHENTICATION TOKEN
      */
     public String getMSIToken() {
-        return getToken("https://management.azure.com/");
+        return getToken("https://management.azure.com/.default");
     }
 
     /**
